fa2b38fbb014680f14f2cbb1e6213f84eb849d2d
Dennis Lydiard <dlydiard.away@gmail.com>
2026-07-27 15:40:28 +0000

    chore: bump version to a.1.0-109

--- README.md
+++ README.md
@@ -318,4 +318,4 @@ pytest src/tests
 
 ## Version
 
-Current version: `a.1.0-108` (Alpha)
+Current version: `a.1.0-109` (Alpha)
--- src/server/lifecycle.py
+++ src/server/lifecycle.py
@@ -13,7 +13,7 @@ import utils.logger as logger
 import utils.config as config
 import utils.version as version
 
-VERSION = "a.1.0-108"
+VERSION = "a.1.0-109"
 
 
 class PlexichatServer:
